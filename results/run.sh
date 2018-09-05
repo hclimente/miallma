952c30d0b7830b2278d98a8bd39756a002aa6e73
hclimente <hclimente@gmail.com>
2018-09-05 11:16:06 +0200

    add profile cluster

--- results/run.sh
+++ results/run.sh
@@ -1,3 +1,3 @@
-#../scripts/0.1_prepare_expression.nf -resume
-#../scripts/0.2_get_drivers.nf -resume
-../scripts/1_run_spada.nf -resume
+../scripts/0.1_prepare_expression.nf -resume -profile quickie
+../scripts/0.2_get_drivers.nf -resume -profile quickie
+../scripts/1_run_spada.nf -resume -profile cluster
